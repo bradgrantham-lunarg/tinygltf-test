1a33c127fe3283696e652ea72006a64d8119e607
Brad Grantham <brad@lunarg.com>
2025-05-31 10:38:47 -0700

    initial commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <cstdio>
+
+#define TINYGLTF_IMPLEMENTATION
+#define STB_IMAGE_IMPLEMENTATION
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "tiny_gltf.h"
+
+int main(int argc, char **argv)
+{
+    if(argc != 2) {
+        fprintf(stderr, "usage: %s filename\n", argv[0]);
+        exit(1);
+    }
+
+    using namespace tinygltf;
+
+    for (int i = 0; i < 100; i++) {
+        Model model;
+        TinyGLTF loader;
+        std::string err;
+        std::string warn;
+
+        // bool ret = loader.LoadASCIIFromFile(&model, &err, &warn, argv[1]);
+        bool ret = loader.LoadBinaryFromFile(&model, &err, &warn, argv[1]); // for binary glTF(.glb)
+
+        if (!warn.empty()) {
+            printf("Warn: %s\n", warn.c_str());
+        }
+
+        if (!err.empty()) {
+            printf("Err: %s\n", err.c_str());
+        }
+
+        if (!ret) {
+            printf("Failed to parse glTF\n");
+            return -1;
+        }
+    }
+
+}
